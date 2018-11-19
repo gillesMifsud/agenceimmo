49ecf7c7608d2135cddaf2eee7be98e1a413911f
Gilles MIFSUD <gilles.mifsud@netapsys.fr>
2018-11-19 07:18:10 +0100

    4-25:00

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -42,7 +42,31 @@ class AdminPropertyController extends AbstractController
     }
 
     /**
-     * @Route("/admin/edit/{id}", name="admin.property.edit")
+     * @Route("/admin/property/create", name="admin.property.new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    public function new(Request $request)
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($property); // Property will be tracked from the entity manager which will do the changes
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/edit/{id}", name="admin.property.edit")
      * @param Property $property
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
