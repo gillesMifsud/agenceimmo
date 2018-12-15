120e77fedcd2305e4412d5c7ee3be304a8b94631
Gilles Mifsud <gil.mifsud@gmail.com>
2018-12-15 13:38:27 +0100

    10 Done

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -29,7 +29,8 @@ class PropertyType extends AbstractType
             ->add('options', EntityType::class, [
                 'class' => Option::class,
                 'choice_label' => 'name',
-                'multiple' => true
+                'multiple' => true,
+                'required' => false
             ])
             ->add('imageFile', FileType::class, [
                 'required' => false
