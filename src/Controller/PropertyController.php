d88499a12ca12648c65606d85ea7c47e165ef680
Gilles Mifsud <gil.mifsud@gmail.com>
2018-12-15 14:26:11 +0100

    11 Mail dev todo

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Property;
 use App\Entity\PropertySearch;
+use App\Form\ContactType;
 use App\Form\PropertySearchType;
+use App\Notification\ContactNotification;
 use App\Repository\PropertyRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -71,9 +74,15 @@ class PropertyController extends AbstractController {
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
      * @param Property $property
      * @param string $slug
+     * @param Request $request
+     * @param ContactNotification $notification
      * @return Response
      */
-    public function show(Property $property, string $slug): Response
+    public function show(
+        Property $property,
+        string $slug,
+        Request $request,
+        ContactNotification $notification): Response
     {
         if ($property->getSlug() !== $slug) { // If user try to change url, prevent with redirect to current slug
             return $this->redirectToRoute('property.show', [
@@ -81,9 +90,25 @@ class PropertyController extends AbstractController {
                 'slug' => $property->getSlug()
             ], 301);
         }
+
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Email envoyé avec succès');
+            /*return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ]);*/
+        }
+
         return $this->render('property/show.html.twig', [
             'property' => $property,
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'form' => $form->createView()
         ]);
     }
 
---
+++ src/Notification/ContactNotification.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Notification;
+
+
+use App\Entity\Contact;
+
+class ContactNotification
+{
+
+    public function notify(Contact $contact)
+    {
+
+    }
+}
